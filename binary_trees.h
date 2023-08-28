933fd9e5c1c1645ee9fcfc2442626cb762c649b6
Sufian Hamza <hsufiian@yahoo.com>
2023-08-28 10:02:02 +0000

    header

--- binary_trees.h
+++ binary_trees.h
@@ -27,7 +27,7 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
-ypedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
