5443b2d361b93b33610b80411b76ecf792cc5493
Sufian Hamza <hsufiian@yahoo.com>
2023-08-29 13:54:44 +0000

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,20 +9,30 @@
  * @tree: Pointer to the root node of the binary tree.
  * Return: 1 if the binary tree is perfect, 0 otherwise or if @tree is NULL.
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (tree == NULL)
+int left_height, right_height;
+
+if (tree != NULL)
+{
+return (1);
+}
+else
+{
 return (0);
+}
 
-int left_height = binary_tree_height(tree->left);
-int right_height = binary_tree_height(tree->right);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
 
-if (left_height != right_height)
+if (right_height == left_height)
+{
+return (1);
+}
+else
+{
 return (0);
-
-return (binary_tree_is_perfect(tree->left) &&
-binary_tree_is_perfect(tree->right));
+}
 }
 
 /**
