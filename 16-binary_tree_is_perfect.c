2ae8359208b1b8f452a1a85c9882a64b15dd6997
Sufian Hamza <hsufiian@yahoo.com>
2023-08-29 13:32:00 +0000

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,28 +11,23 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int left_height, right_height;
-
-if (tree != NULL)
-{
-return (1);
-}
-else
-{
+if (tree == NULL)
 return (0);
-}
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
+int left_height = binary_tree_height(tree->left);
+int right_height = binary_tree_height(tree->right);
 
-if (right_height == left_height)
-{
+if (tree->left == NULL && tree->right == NULL)
 return (1);
-}
-else
-{
+
+if (tree->left == NULL || tree->right == NULL)
 return (0);
-}
+
+if (left_height != right_height)
+return (0);
+
+return (binary_tree_is_perfect(tree->left) &&
+binary_tree_is_perfect(tree->right));
 }
 
 /**
