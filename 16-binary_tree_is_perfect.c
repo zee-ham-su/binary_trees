43ca943e90f7eeb672c0ba811117d33442c3621b
Sufian Hamza <hsufiian@yahoo.com>
2023-08-29 14:06:49 +0000

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,29 +11,31 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int left_height, right_height;
+if (tree == NULL)
+return (0);
 
-if (tree != NULL)
+int left_height = binary_tree_height(tree->left);
+int right_height = binary_tree_height(tree->right);
+
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+
+if (tree->left == NULL || tree->right == NULL)
+return (0);
+
+if (left_height != right_height)
+return (0);
+
+if (binary_tree_is_perfect(tree->left))
+{
+if (binary_tree_is_perfect(tree->right))
 {
 return (1);
 }
-else
-{
+}
 return (0);
 }
 
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-
-if (right_height == left_height)
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
 
 /**
  * binary_tree_height - Calculate the height of a binary tree.
