eb513d644131ee1cd2ef2f117e57261e1928f6e1
Sufian Hamza <hsufiian@yahoo.com>
2023-08-29 14:19:33 +0000

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,43 +4,39 @@
 
 /**
  * binary_tree_is_perfect - Check if a binary tree is perfect.
- * A binary tree is considered perfect if all of its levels are completely
- * filled and the tree is full (every node has either 0 or 2 children).
  * @tree: Pointer to the root node of the binary tree.
- * Return: 1 if the binary tree is perfect, 0 otherwise or if @tree is NULL.
+ * Return: 1 if the binary tree is perfect,
+ * 0 otherwise or if @tree is NULL.
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
+int left_height, right_height;
 
-int left_height = binary_tree_height(tree->left);
-int right_height = binary_tree_height(tree->right);
-
-if (tree->left == NULL && tree->right == NULL)
-return (1);
-
-if (tree->left == NULL || tree->right == NULL)
-return (0);
-
-if (left_height != right_height)
-return (0);
-
-if (binary_tree_is_perfect(tree->left))
-{
-if (binary_tree_is_perfect(tree->right))
+if (tree != NULL)
 {
 return (1);
 }
-}
+else
+{
 return (0);
 }
 
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+
+if (right_height == left_height)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
 
 /**
  * binary_tree_height - Calculate the height of a binary tree.
- * The height of a binary tree is the number of edges on the longest path
- * from the root node to a leaf node.
  * @tree: Pointer to the root node of the binary tree.
  * Return: Height of the binary tree. 0 if @tree is NULL.
  */
