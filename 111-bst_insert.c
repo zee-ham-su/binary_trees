808d55df67568634ffb465b40943e90a4a8e63e4
Sufian Hamza <hsufiian@yahoo.com>
2023-08-30 14:18:43 +0000

    bst insert

---
+++ 111-bst_insert.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - Inserts a value in a Binary Search Tree
+ * @tree: Double pointer to the root node of the BST to insert the value
+ * @value: Value to insert into the tree
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+bst_t *new_node = NULL;
+
+if (tree == NULL)
+return (NULL);
+
+if (*tree == NULL)
+{
+new_node = binary_tree_node(NULL, value);
+*tree = new_node;
+return (new_node);
+}
+
+bst_t *current_node = *tree;
+
+while (current_node)
+{
+if (value < current_node->n)
+{
+if (current_node->left == NULL)
+{
+new_node = binary_tree_node(current_node, value);
+current_node->left = new_node;
+return (new_node);
+}
+current_node = current_node->left;
+}
+else
+{
+if (current_node->right == NULL)
+{
+new_node = binary_tree_node(current_node, value);
+current_node->right = new_node;
+return (new_node);
+}
+current_node = current_node->right;
+}
+}
+
+return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -59,9 +59,9 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
-
-	/* Aux function prototypes */
+/* Aux function prototypes */
 void traverse_level(const binary_tree_t *tree, void (*func)(int), size_t target_level);
 int is_complete_recursive(const binary_tree_t *tree,
 								  size_t index, size_t node_count);
